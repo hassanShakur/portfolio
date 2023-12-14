3bf74c56ec8e762fa63a850754ab688c46769025
hassanShakur <hass.shakurhassan@gmail.com>
2023-12-14 15:06:48 +0300

    add first form setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,24 @@ export default function RootLayout({
         <body>
           <Header />
           {children}
+          {/* Contact form */}
+          <form name='contact' id='contact'>
+            <div>
+              <label htmlFor='name'>Name</label>
+              <input type='text' name='name' id='name' />
+            </div>
+            <div>
+              <label htmlFor='email'>Email</label>
+              <input type='email' name='email' id='email' />
+            </div>
+            <div>
+              <label htmlFor='message'>Message</label>
+              <textarea name='message' id='message' rows={5} />
+            </div>
+            <div>
+              <button type='submit'>Send</button>
+            </div>
+          </form>
           <Footer />
         </body>
         {/* <body className={inter.className}>{children}</body> */}
