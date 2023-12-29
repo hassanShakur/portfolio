062b63bd2e5eec08798b722f027f58f41f1c0179
hassanShakur <hass.shakurhassan@gmail.com>
2023-12-29 15:25:58 +0300

    Add ContactForm component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.scss';
 import store from '@/redux/store';
 import { Provider } from 'react-redux';
 import Footer from '@/components/footer';
+import ContactForm from '@/components/contact/Form';
 // import { Inter } from 'next/font/google'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -20,6 +21,7 @@ export default function RootLayout({
         <body>
           <Header />
           {children}
+          <ContactForm />
           <Footer />
         </body>
         {/* <body className={inter.className}>{children}</body> */}
