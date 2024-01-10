4c66ace6dab5072ee1743f2954070be401efa074
hassanShakur <hass.shakurhassan@gmail.com>
2024-01-10 11:29:15 +0300

    initial light styling

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image';
 
-import myLogoImg from  '@/images/logo.png'
-import './logo.scss'
-
+import myLogoImg from '@/images/logo.png';
+import './logo.scss';
 
 const AnimatedLogo = () => {
-  return <div id='animated-logo'>
-    <Image src={myLogoImg} alt="Hassan Shakur Logo" width={200} height={200} />
-  </div>;
+  return (
+    <div id='animated-logo'>
+      <div className='logo-border'>
+        <Image
+          src={myLogoImg}
+          alt='Hassan Shakur Logo'
+          // width={200}
+          // height={200}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AnimatedLogo;
